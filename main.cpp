e82b1b0e03777b7082310ece1f81e1679db4a2d1
Findof <joeargon21@gmail.com>
2024-06-13 09:33:51 -0400

    Init commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "External/stb_image.h"
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
+#include <filesystem>
+
+int main()
+{
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
